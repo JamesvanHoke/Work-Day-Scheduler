e9ca604f95018ac052fbc459861aedeac56d511e
James V <james.vanhoke@yahoo.com>
2021-03-08 01:38:31 -0700

    Local Storage basic functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,40 @@
-//Variables
+//////////Variables//////////
 
-// Variable that represents date in Day(Mon-Fri), Month Xth format. ex. Sunday, March 7th using the Moment.js date library utility.
-var date = moment().format('dddd, MMMM Do')
+//Moment.js Time Variables
 
-//Assigns our <p> at the top of the page a variable so we can use moment.js to update the date.
-var currentDay = document.getElementById("currentDay")
+//Represents date in Day(Mon-Fri), Month Xth format. ex. Sunday, March 7th using the Moment.js date library utility.
+var date = moment().format("dddd, MMMM Do");
 
-//Sets our above <p> to be equal to todays date
-currentDay.textContent = date   
+//Represents the current hour in 1-24 format
+var time = moment().format("HH");
 
+//Header Date Variable
+var currentDay = $("#currentDay");
+
+//////////Save Function//////////
+
+//Targets the time block div and listens for a click
+$(".time-block").on("click", function (e) {
+  //Guard clause to make sure that a button is clicked
+  if (!e.target.matches("button")) {
+      //exits out if a button isn't clicked
+    return;
+  }
+//Jquery DOM manipulation to get the user input from the text area equal to the 
+var textAreaInput = $(this).children(".description").val()
+//jQuery DOM manipulation to get the ID of the clicked
+var hourLS = $(this).attr("id");
+
+localStorage.setItem(hourLS, textAreaInput)
+
+});
+
+$("#hour-1 .description").val(localStorage.getItem("hour-1"));
+
+
+
+//Uses Moment.js to assign our current day <p> the the date upon loading.
+currentDay.text(date);
 
 //Current day is displayed at the top of the calendar✅
 //Below that is a series of time blocks✅
